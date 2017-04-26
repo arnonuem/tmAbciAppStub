0c4dc78d5df93e906d0f4b8a82a217cbf00ec878
arnonuem <arno.nuem@protonmail.com>
2017-04-26 19:32:18 +0200

    cleanups
    port of app changed to 8090
    added default replies for tendermint rpc calls

--- src/main/java/net/arnonuem/tmstub/TSockListener.java
+++ src/main/java/net/arnonuem/tmstub/TSockListener.java
@@ -3,7 +3,18 @@ package net.arnonuem.tmstub;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.jtendermint.jabci.api.ABCIAPI;
+import com.github.jtendermint.jabci.api.IBeginBlock;
+import com.github.jtendermint.jabci.api.ICheckTx;
+import com.github.jtendermint.jabci.api.ICommit;
+import com.github.jtendermint.jabci.api.IDeliverTx;
+import com.github.jtendermint.jabci.api.IEcho;
+import com.github.jtendermint.jabci.api.IEndBlock;
+import com.github.jtendermint.jabci.api.IFlush;
+import com.github.jtendermint.jabci.api.IInfo;
+import com.github.jtendermint.jabci.api.IInitChain;
+import com.github.jtendermint.jabci.api.IQuery;
+import com.github.jtendermint.jabci.api.ISetOption;
+import com.github.jtendermint.jabci.types.Types.CodeType;
 import com.github.jtendermint.jabci.types.Types.RequestBeginBlock;
 import com.github.jtendermint.jabci.types.Types.RequestCheckTx;
 import com.github.jtendermint.jabci.types.Types.RequestCommit;
@@ -27,86 +38,74 @@ import com.github.jtendermint.jabci.types.Types.ResponseInitChain;
 import com.github.jtendermint.jabci.types.Types.ResponseQuery;
 import com.github.jtendermint.jabci.types.Types.ResponseSetOption;
 
-public class TSockListener implements ABCIAPI {
+//TODO workaround since there is an issue with ABCIAPI
+public class TSockListener /*implements ABCIAPI*/implements IDeliverTx, IBeginBlock, ICheckTx, ICommit, IEndBlock, IFlush, IInfo, IInitChain, IQuery, ISetOption, IEcho {
 
 	private static final Logger log = LoggerFactory.getLogger( TSockListener.class );
-
-	@Override
-	public ResponseDeliverTx receivedDeliverTx(RequestDeliverTx arg0) {
-		log.debug( "receivedDeliverTx" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseBeginBlock requestBeginBlock(RequestBeginBlock arg0) {
-		log.debug( "requestBeginBlock" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseCheckTx requestCheckTx(RequestCheckTx arg0) {
-		log.debug( "requestCheckTx" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseCommit requestCommit(RequestCommit arg0) {
-		log.debug( "requestCommit" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseEndBlock requestEndBlock(RequestEndBlock arg0) {
-		log.debug( "requestEndBlock" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseFlush requestFlush(RequestFlush arg0) {
-		log.debug( "requestFlush" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseInfo requestInfo(RequestInfo arg0) {
-		log.debug( "requestInfo" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseInitChain requestInitChain(RequestInitChain arg0) {
-		log.debug( "requestInitChain" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseQuery requestQuery(RequestQuery arg0) {
-		log.debug( "requestQuery" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseSetOption requestSetOption(RequestSetOption arg0) {
-		log.debug( "requestSetOption" );
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResponseEcho requestEcho(RequestEcho arg0) {
-		log.debug( "requestEcho" );
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
+    @Override
+    public ResponseDeliverTx receivedDeliverTx(RequestDeliverTx req) {
+        log.debug("ResponseDeliverTx DefaultFallbackListener");
+        return ResponseDeliverTx.newBuilder().setCode(CodeType.OK).build();
+    }
 
+    @Override
+    public ResponseFlush requestFlush(RequestFlush reqfl) {
+        log.debug("ResponseFlush DefaultFallbackListener");
+        return ResponseFlush.newBuilder().build();
+    }
+
+    @Override
+    public ResponseCommit requestCommit(RequestCommit requestCommit) {
+        log.debug("ResponseCommit DefaultFallbackListener");
+        return ResponseCommit.newBuilder().setCode(CodeType.OK).build();
+    }
+
+    @Override
+    public ResponseBeginBlock requestBeginBlock(RequestBeginBlock req) {
+        log.debug("ResponseBeginBlock DefaultFallbackListener");
+        return ResponseBeginBlock.newBuilder().build();
+    }
+
+    @Override
+    public ResponseCheckTx requestCheckTx(RequestCheckTx req) {
+        log.debug("ResponseCheckTx DefaultFallbackListener");
+        return ResponseCheckTx.newBuilder().setCode(CodeType.OK).build();
+    }
+
+    @Override
+    public ResponseEndBlock requestEndBlock(RequestEndBlock req) {
+        log.debug("ResponseEndBlock DefaultFallbackListener");
+        return ResponseEndBlock.newBuilder().build();
+    }
+
+    @Override
+    public ResponseInfo requestInfo(RequestInfo req) {
+        log.debug("ResponseInfo DefaultFallbackListener");
+        return ResponseInfo.newBuilder().setData("NO_INFO").build();
+    }
+
+    @Override
+    public ResponseInitChain requestInitChain(RequestInitChain req) {
+        log.debug("ResponseInitChain DefaultFallbackListener");
+        return ResponseInitChain.newBuilder().build();
+    }
+
+    @Override
+    public ResponseQuery requestQuery(RequestQuery req) {
+        log.debug("ResponseQuery DefaultFallbackListener");
+        return ResponseQuery.newBuilder().setCode(CodeType.OK).build();
+    }
+
+    @Override
+    public ResponseSetOption requestSetOption(RequestSetOption req) {
+        log.debug("ResponseSetOption DefaultFallbackListener");
+        return ResponseSetOption.newBuilder().build();
+    }
+
+    @Override
+    public ResponseEcho requestEcho(RequestEcho req) {
+        log.debug("ResponseEcho DefaultFallbackListener");
+        return ResponseEcho.newBuilder().setMessage("NOECHO").build();
+    }
 }
