072c918f7aaf514a6830751003f3c0a7a46fb599
arnonuem <arno.nuem@protonmail.com>
2017-04-27 22:28:13 +0200

    added license

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # tmAbciAppStub
-tendermint abci java app stub
+Tendermint ABCI Java application stub for rapdid prototyping Tendermint apps.
+
+## frameworks
+The application is based on Spring Boot and JTendermint.
+
+## run
 
 Run the ABCI-App. It should open a Tendermint socket and wait for Tendermint connecting to it.
 
 Run Tendermint `tendermint node` (see Tendermint docs). Tendermint should connect to the ABCI-App.
+
+## development
+All requests made by Tendermint are mapped into services, except `echo` and `flush`.
+When implementing the desired app just wire the services and replace the `noop` methods with the desired content. 
--- src/main/java/net/arnonuem/tmstub/sys/InfoQueryListener.java
+++ src/main/java/net/arnonuem/tmstub/sys/InfoQueryListener.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 - 2018
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package net.arnonuem.tmstub.sys;
 
 import com.github.jtendermint.jabci.api.IInfo;
@@ -13,6 +36,8 @@ import com.github.jtendermint.jabci.api.ISetOption;
  * Tendermint Core currently uses the Query connection to filter peers upon connecting, according to IP address or public key.
  * 
  * https://tendermint.com/docs/guides/app-development
+ * 
+ * @author arnonuem
  */
 public interface InfoQueryListener extends IInfo, ISetOption, IQuery {
 
